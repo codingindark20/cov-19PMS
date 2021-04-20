7d268c4ff4f23e04cf613b05bce943932a5f1c55
Codingindark <rochakrupakheti20@gmail.com>
2021-04-20 06:41:16 +0545

    Setting and chnaging patient status & statistical information system created

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def patientRegistration():
         priorityGrp = input("Enter priority group among ATO/ACC/AEO/SID/AHS : ")
         
         #Writing data to the file
+        file.write("Data\t")
         file.write(str(id))
         file.write("\t"+patientName)
         file.write("\t"+patientAge)
@@ -49,7 +50,9 @@ def patientRegistration():
 
         else:
             print("You entered invalid Priority Group.")
-    
+            return
+#END OF FUNCTION     
+
 
 
 
@@ -61,8 +64,9 @@ def testSID():
         file.write("\tT1 : "+T1result)
         if T1result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
             print("Patient is in : "+NWICU)
+            file.write("\tACTIVE\n")
         elif T1result == "-ve":
             file.write("\tHQFR")
             print("Patient is in Home Quarantine.\n")
@@ -75,7 +79,8 @@ def testSID():
         file.write("\tT2 : "+T2result)
         if T2result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
+            file.write("\tACTIVE\n")
             print("Patient is in : "+NWICU)
         elif T2result == "-ve":
             file.write("\tHQFR")
@@ -89,16 +94,19 @@ def testSID():
         file.write("\tT3 : "+T3result)  
         if T3result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
+            file.write("\tACTIVE\n")
             print("Patient is in : "+NWICU)
         elif T3result == "-ve":
-            file.write("\tRU\n")
+            file.write("\tRU")
             print("Patient is in Home")
+            file.write("\tRECOVERED\n")
         else:
             print("Sorry! Invalid Input.")
     
     T1SID()
-        
+#END OF FUNCTION         
+
 
 
 #Function for AHS (Asymptomatic Hospital Staff)
@@ -108,7 +116,8 @@ def testAHS():
         T1result = input("Enter Test1 result (+ve/-ve) : ")
         file.write("\tT1 : "+T1result)
         if T1result == "+ve":
-            file.write("\tHQNF\n")
+            file.write("\tHQNF")
+            file.write("\tACTIVE\n")
             print("Patient is in Home Quarantine.")
         elif T1result == "-ve":
             file.write("\tCWFR")
@@ -121,7 +130,8 @@ def testAHS():
         T2result = input("Enter Test2 result (+ve/-ve) : ")
         file.write("\tT2 : "+T2result)
         if T2result == "+ve":
-            file.write("\tHQNF\n")
+            file.write("\tHQNF")
+            file.write("\tACTIVE\n")
             print("Patient is in Home Quarantine.\n")
         elif T2result == "-ve":
             file.write("\tCWFR")
@@ -135,15 +145,18 @@ def testAHS():
         T3result = input("Enter Test3 result (+ve/-ve) : ")
         file.write("\tT3 : "+T3result)
         if T3result == "+ve":
-            file.write("\tHQNF\n")
+            file.write("\tHQNF")
+            file.write("\tACTIVE\n")
             print("Patient is in Home Quarantine.")
         elif T3result == "-ve":
-            file.write("\tCW\n")
+            file.write("\tCW")
+            file.write("\tRECOVERED\n")
             print("Continue working.")
         else:
             print("Sorry! Invalid Input.")
      
     T1AHS()
+#END OF FUNCTION 
 
 
 
@@ -156,7 +169,8 @@ def testrslt():
         file.write("\tT1 : "+T1result)
         if T1result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
+            file.write("\tACTIVE\n")
             print("Patient is in : "+NWICU)
         elif T1result == "-ve":
             file.write("\tQDFR")
@@ -170,7 +184,8 @@ def testrslt():
         file.write("\tT2 : "+T2result)
         if T2result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
+            file.write("\tACTIVE\n")
             print("Patient is in : "+NWICU)
         elif T2result == "-ve":
             file.write("\tQDFR")
@@ -186,30 +201,87 @@ def testrslt():
         file.write("\tT3 : "+T3result)  
         if T3result == "+ve":
             NWICU = input("Enter the quarantine of the patient (NW/ICU) : ")
-            file.write("\t"+NWICU+"\n")
+            file.write("\t"+NWICU)
+            file.write("\tACTIVE\n")
             print("Patient is in : "+NWICU)
         elif T3result == "-ve":
-            file.write("\tRU\n")
+            file.write("\tRU")
+            file.write("\tRECOVERED\n")
             print("Patient is in Home\n")
         else:
             print("Sorry! Invalid Input.")
 
     T1()
+#END OF FUNCTION 
 
 
 
 
 #This is function for Setting and Changing Patient Status(ACTIVE/RECOVERED/DECEASED)
+import sys
+import fileinput
 def settingChng():
-    print("\tSetting and Changing Patient Status\n")
- 
+    print("\n\tSetting and Changing Patient Status\n")
+    # with is like your try .. finally block in this case
+    with open('data.txt') as file:
+
+# read a list of lines into data
+        data = file.read()
+
+    print(data)
+    
+    print("\nYou can change case status ACTIVE to RECOVERED\n")
+    lineno = int(input("Enter 1 to change case status ACTIVE to RECOVERD : "))
+    if lineno == 1:
+
+# replace all occurrences of 'ACTIVE' with 'RECOVERED' 
+        for i, line in enumerate(fileinput.input('data.txt', inplace=1)):
+            sys.stdout.write(line.replace('ACTIVE', 'RECOVERED'))  # replace 'sit' and write
+
+    print("\nData changed succesfully!\n")
+    view = int(input("Press 1 to view new data : "))
+    if view == 1:
+        with open('data.txt') as file:
+            newdata = file.read()
+            print(newdata)
+
+#END OF FUNCTION       
+
 
 
 
 #This is Function for Statiscal Information on Tests Carried Out
 def statsInfo():
-    print("\tStatistical Information\n")
+    print("\n\tStatistical Information\n")
+    
+    with open("data.txt") as f:
+        readfile = f.read()
 
+
+    totalTests = 0
+    PatientTested = 0
+    RecoveredCases = 0
+    ActiveCases = 0
+
+    
+    for line in readfile:
+        stats = readfile.split(",")
+        if stats == 'T1' or 'T2' or 'T2' :
+            totalTests += 1
+        elif stats == 'Data' :
+            PatientTested += 1
+        elif stats == 'ACTIVE' :
+            ActiveCases += 1
+        elif stats == 'RECOVERED':
+            RecoveredCases += 1
+
+    print("There are ", totalTests, " Total Tests done.")
+    print("There are ", PatientTested, " patient tested.")
+    print("There are ", ActiveCases, " active cases.")
+    print("There are ", RecoveredCases, " recovered cases.")
+#END OF FUNCTION
+
+    
 
 
 #This function is for Searching Functionalities
@@ -242,11 +314,11 @@ def searching():
 
     else:
         print("Sorry! your input is wrong! Please try again.")
-
+#END OF FUNCTION
 
 
 #Output window starts here
-print("\t\tCOVID-19 Patient Mangement System\n")
+print("\t\tCOVID-19 Patient Management System\n")
 print("1. New Patient Registration\n2. Setting and Changing Patient Status\n3. Statistical Information\n4. Search\n ")
 option = int(input("Choose a option (1/2/3/4) : "))
 
